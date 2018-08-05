60e3e4d98535e96be3e99a413f527f441a23fff6
DerekStevens99 <38191099+DerekStevens99@users.noreply.github.com>
2018-08-05 11:24:48 -0400

    User 2.0
    User is now a password, username, and an array of questions.

--- User.java
+++ User.java
@@ -1,23 +1,20 @@
+package minimum;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class User {
 
 	/* Fields */
-	String name;
-	String password;
-	int[] responseIDs = new int[4]; 
+	private String name;
+	private String password;
+	private ArrayList<Question> questions = new ArrayList<Question>();
 
 	/* Constructor */
-	public User(String thisName; String thisPassword; int ID1; int ID2; int ID3; int ID4) {
-		name = thisName;
-		password = thisPassword;
-		responseIDs[0] = ID1;
-		responseIDs[0] = ID1;
-		responseIDs[0] = ID1;    // Not sure how to loop through variables in a constructor
-		responseIDs[0] = ID1;
-	}
-
+	public User(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}	
+	
 	/* Setter Methods */
 	public void setName(String newName) {
 		name = newName;
@@ -27,9 +24,6 @@ public class User {
 		password = newPassword;
 	}
 
-	public void setResponseID(int index, int newID) {
-		responseIDs[index] = newID;
-	}
 
 	/* Getter Methods */
 	public String getName() {
@@ -39,8 +33,14 @@ public class User {
 	public String getPassword() { 
 		return password;
 	}
-
-	public int getResponseID(int index) {
-		return responseIDs[index];
+	
+	
+	public static void main(String args[]) {
+		User derek = new User("derek", "password");
+		System.out.println(derek.getName());
+		Question quest = new Question("Where do you live?");
+		quest.answer();
+		System.out.println(quest.get_Question_answers().get(1));
 	}
+
 }
